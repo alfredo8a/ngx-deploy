6013f665a38b68007eba6b3637d65f224fcbb015
Alfredo Ochoa Lopez <vn0e85u@email.wal-mart.com>
2019-05-13 15:04:11 -0500

    Done v1 ngx-deploy

--- src/ng-add/index.ts
+++ src/ng-add/index.ts
@@ -1,17 +1,58 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, chain } from '@angular-devkit/schematics';
+import { getWorkspace, getProjectFromWorkspace, getProjectTargets } from 'schematics-utilities';
+// import { getWorkspace, getProjectFromWorkspace, getProjectTargets, getWorkspacePath } from 'schematics-utilities';
 
+export default function (): Rule {
+  return chain([
+    createDeployEnviroments(),
+    // updateFileReplacements(),
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export default function(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    const content = {host:'',user:'',path:''}
-    const envs = tree.getDir('src/environments').subfiles
-    .filter(item => !item.endsWith('.map') && !item.endsWith('d.ts') && !item.endsWith('.js'))
-    .map(item => {
-      item.replace('environment','deploy')
-      tree.create(`src/environments/${item}`,JSON.stringify(content))
+  ]);
+}
+// function updateFileReplacements(): Rule {
+//   return (host: Tree, _context: SchematicContext) => {
+//     const workspace = getWorkspace(host);
+//     // const recorder : UpdateRecorder = {insertLeft(),insertRight()}
+//     // appendPropertyInAstObject({insertLeft(0,'deploy')},workspace,'architect')
+//     const project = getProjectFromWorkspace(workspace, workspace['defaultProject']);
+//     var targets = getProjectTargets(project);
+
+//     targets.map(target => target.configurations).map(config => {
+//       config.fileReplacements.push()
+//     })
+//     host.overwrite(getWorkspacePath(host), JSON.stringify(workspace, null, 2));
+
+//     // addProjectToWorkspace(workspace,'deploy',project)
+//     // console.log(targets);
+
+//     return host;
+//   };
+// }
+
+function createDeployEnviroments(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const workspace = getWorkspace(tree);
+    const project = getProjectFromWorkspace(workspace);
+    const targets = getProjectTargets(project);
+
+    const keys = Object.keys(targets.build.configurations);
+    const arrayJson = {}
+    keys.forEach(key => {
+      arrayJson[key] = {
+        source: targets.build.configurations[key].outputPath,
+        host: '',
+        user: '',
+        password: '',// Optional if privateKey = true
+        privateKey: false,
+        path: ''
+      }
     });
+
+    if (!tree.exists('src/environments/deploy.json')) {
+      tree.create('src/environments/deploy.json', JSON.stringify(arrayJson, null, '\t'));
+    } else {
+      context.logger.error('FILE EXIST ' + 'src/environments/deploy.json');
+    }
     return tree;
   };
 }
